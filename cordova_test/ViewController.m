956ca54f97e0e9d17ead52b8b52b428aae2f1fb5
tuzhengyao <905430956@qq.com>
2016-06-16 11:51:33 +0800

    提交修改

--- cordova_test/ViewController.m
+++ cordova_test/ViewController.m
@@ -39,14 +39,16 @@
    [self.navigationController pushViewController:self.cordovaManageVC animated:YES];
 }
 
-//- (void)viewWillAppear:(BOOL)animated
-//{
-//    [super viewWillAppear:animated];
-//    self.navigationController.navigationBar.hidden = NO;
-//}
-//- (void)viewWillDisappear:(BOOL)animated
-//{
-//    [super viewWillDisappear:animated];
-//    self.navigationController.navigationBar.hidden = YES;
-//}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBar.hidden = NO;
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    self.navigationController.navigationBar.hidden = YES;
+}
+
 @end
